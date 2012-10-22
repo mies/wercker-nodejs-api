16e364ef872a620e2a855609c65b04b17f0b6e2c
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2012-10-22 13:51:10 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express.createServer();
+
+app.get('/', function(req, res){
+  res.send("Hello Cybertron!")
+});
+
+app.get('/insecticons.json', function(req, res){
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.write(JSON.stringify({insecticons : ["foo","bar"]}));
+  res.end();
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
